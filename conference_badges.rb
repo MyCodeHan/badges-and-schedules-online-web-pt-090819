ff63fdf7dd5c22d8d18fbbc6b4df2874c561b2a8
Hannah Harrington <hannahharrington@outlook.com>
2019-09-23 12:41:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def conference_badges
+
+  print name {"Arel"}
+  print attendees {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+  print badges {[
+                  "Hello, my name is Edsger.",
+                  "Hello, my name is Ada.",
+                  "Hello, my name is Charles.",
+                  "Hello, my name is Alan.",
+                  "Hello, my name is Grace.",
+                  "Hello, my name is Linus.",
+                  "Hello, my name is Matz."
+               ]}
+
+  let(:room_assignments) {[
+                            "Hello, Edsger! You'll be assigned to room 1!",
+                            "Hello, Ada! You'll be assigned to room 2!",
+                            "Hello, Charles! You'll be assigned to room 3!",
+                            "Hello, Alan! You'll be assigned to room 4!",
+                            "Hello, Grace! You'll be assigned to room 5!",
+                            "Hello, Linus! You'll be assigned to room 6!",
+                            "Hello, Matz! You'll be assigned to room 7!"
+                         ]}
+                         
+                       end
+                       end
